641247a17a48c228e7a8db9e88e53c11807481de
Suporte <junioralbino741@gmail.com>
2022-05-13 17:15:51 -0300

    Entrado no pricibio solid

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,46 @@
+import { prisma } from "./prisma";
+import nodemailer from 'nodemailer';
 import express from "express";
 
 
 
 const app = express();
 
-app.get('/users', (req, res) => {
-   res.send('Hello Word junior')
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "692c8a44c56341",
+    pass: "7c2cde39bc7fec"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot}  = req.body;
+
+  const feedback = await prisma.feedback.create({
+      data:  {
+        type,
+        comment,
+        screenshot
+      }
+  })
+  
+  await transport.sendMail({
+    from: 'Equipe do feeedget <oi@feedget.com>',
+    to: 'Junior Albino <junior@gmail.com>',
+    subject: 'Novo feedback enviando pelo usuario',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111; ">`,
+      `<p>Tipo de feedback: ${type} </p>`,
+      `<p>Comentário: ${comment} </p>`,
+      `</div>`,
+    ].join('\n')
+  })
+
+  return res.status(201).json({ data: feedback})
 });
 
 app.listen(3333, () => {
